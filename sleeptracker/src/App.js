5d7709ffa163713b17125accd3c0d29d1ffebbb2
Daniel Martin <danielmartin1205@gmail.com>
2019-09-18 20:05:25 -0400

    added components folder, login page, dependencies

--- sleeptracker/src/App.js
+++ sleeptracker/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+import Login from './Components/Login';
 
 function App() {
+  const [user, setUser] = useState("");
   return (
     <div className="App">
-
+      <Route exact path='/' render={props => <Login {...props} user={user} /> } />
     </div>
   );
 }
